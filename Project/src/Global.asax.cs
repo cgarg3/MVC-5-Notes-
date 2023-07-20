50ed778f22987a765650d5f7b4d2dd65220a1e60
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-07-20 05:19:55 -0400

    Update Global.asax.cs

--- Project/src/Global.asax.cs
+++ Project/src/Global.asax.cs
@@ -1,1 +1,16 @@
+using System.Web;
+using System.Web.Mvc;
 
+namespace HPlus.Ecommerce
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
